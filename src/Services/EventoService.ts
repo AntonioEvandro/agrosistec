0765fb55b846bbdc057a5d91b0fe442fd6b1b442
AntonioEvandro <evandroalves123321@gmail.com>
2023-09-06 18:29:39 -0300

    Update EventoService.ts

---
+++ src/Services/EventoService.ts
@@ -0,0 +1,12 @@
+import { EventoRepository } from "../Repositories/EventoRepository";
+
+export class EventoService {
+  private eventoRepository: EventoRepository;
+
+  constructor() {
+    this.eventoRepository = new EventoRepository();
+  }
+  
+  // Metodo para criar um novo evento
+  
+}
