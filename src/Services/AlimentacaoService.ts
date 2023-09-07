4fd2385fb8970be912791fe929097b101ab20d5d
AntonioEvandro <evandroalves123321@gmail.com>
2023-09-06 23:37:20 -0300

    Create AlimentacaoService.ts

---
+++ src/Services/AlimentacaoService.ts
@@ -0,0 +1,57 @@
+import { AlimentacaoRepository } from "../Repositories/AlimentacaoRepository";
+
+export class AlimentacaoService {
+  private alimentacaoRepository: AlimentacaoRepository;
+
+  constructor() {
+    this.alimentacaoRepository = new AlimentacaoRepository();
+  }
+  
+  // Metodo para criar uma nova alimentacao
+  async createAlimentacao(data: any) {
+    try {
+      // Chama o método createAlimentacao do AlimentacaoRepository para criar uma nova alimentacao com os dados fornecidos
+      return await this.alimentacaoRepository.createAlimentacao(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  // Método para atualizar uma alimentacao existente
+  async updateAlimentacao(alimentacaoId: string, updatedData: any) {
+    try {
+      return await this.alimentacaoRepository.updateAlimentacao(alimentacaoId, updatedData);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Método para obter todos as alimentacoess
+  async getAlimentacao() {
+    try {
+      // Chama o método findAllAlimentacao do AlimentacaoRepository para obter todas as alimentacoes cadastradas
+      return await this.alimentacaoRepository.findAllAlimentacao();
+    } catch (error) {
+      // Em caso de erro, lança o erro para ser tratado posteriormente
+      throw error;
+    }
+  }
+
+  // Método para obter uma alimentacao específica pelo ID
+  async getAlimentacaoById(alimentacaoId: string) {
+    try {
+      return await this.alimentacaoRepository.findAlimentacaoById(alimentacaoId);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Método para deletar uma alimentacao pelo ID
+  async deleteAlimentacao(alimentacaoId: string) {
+    try {
+      return await this.alimentacaoRepository.deleteAlimentacao(alimentacaoId);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
