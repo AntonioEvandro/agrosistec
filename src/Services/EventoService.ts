f6a67ebcf09ab536e319950681bc8a5382e34bea
AntonioEvandro <evandroalves123321@gmail.com>
2023-09-06 23:21:19 -0300

    Update EventoService.ts

--- src/Services/EventoService.ts
+++ src/Services/EventoService.ts
@@ -17,4 +17,41 @@ export class EventoService {
     }
   }
   
+  // Método para atualizar um evento existente
+  async updateEvento(eventoId: string, updatedData: any) {
+    try {
+      return await this.eventoRepository.updateEvento(eventoId, updatedData);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Método para obter todos os animais
+  async getEvento() {
+    try {
+      // Chama o método findAllAnimais do EventoRepository para obter todos os animais cadastrados
+      return await this.eventoRepository.findAllEvento();
+    } catch (error) {
+      // Em caso de erro, lança o erro para ser tratado posteriormente
+      throw error;
+    }
+  }
+
+  // Método para obter um evento específico pelo ID
+  async getEventoById(eventoId: string) {
+    try {
+      return await this.eventoRepository.findEventoById(eventoId);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Método para deletar um evento pelo ID
+  async deleteEvento(eventoId: string) {
+    try {
+      return await this.eventoRepository.deleteEvento(eventoId);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
