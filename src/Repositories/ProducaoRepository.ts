54155bc3d065a23f511c3a402e9018306afe2fc2
AntonioEvandro <evandroalves123321@gmail.com>
2023-09-09 09:17:40 -0300

    Update ProducaoRepository.ts
    Adicionado o método deleteProducao

--- src/Repositories/ProducaoRepository.ts
+++ src/Repositories/ProducaoRepository.ts
@@ -20,4 +20,32 @@ export class ProducaoRepository {
     }
   }
 
+  // Método para obter todas as producoes cadastradas no banco de dados
+  async findAllProducao() {
+    try {
+      // Chama o método find do modelo producao para obter todas as producoes cadastradas
+      return await Producao.find();
+    } catch (error) {
+      // Em caso de erro, lança o erro para ser tratado posteriormente
+      throw error;
+    }
+  }
+  
+  // Método para obter uma producao específica pelo ID no banco de dados
+  async findProducaoById(producaoId: string) {
+    try {
+        return await Producao.findById(producaoId);
+    } catch (error) {
+        throw error;
+    }
+  }
+
+  // Método para deletar uma producao pelo ID no banco de dados
+  async deleteProducao(producaoId: string) {
+    try {
+        return await Producao.findByIdAndDelete(producaoId);
+    } catch (error) {
+        throw error;
+    }
+  }
 }
